1a4951cd27d7425a6aabcb9fd18b49d283f76215
Makram <M7MDMKRM.0@gmail.com>
2026-08-30 00:28:34 +0300

    Auto-focus and listen for global barcode scanner inputs in POS terminal

--- AxonPOS/Axon.UI/Views/PosTerminalView.xaml.cs
+++ AxonPOS/Axon.UI/Views/PosTerminalView.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows.Controls;
+using System.Windows.Input;
 using Axon.UI.ViewModels;
 
 namespace Axon.UI.Views
@@ -9,6 +10,7 @@ namespace Axon.UI.Views
         {
             InitializeComponent();
             Loaded += PosTerminalView_Loaded;
+            PreviewKeyDown += PosTerminalView_PreviewKeyDown;
         }
 
         private async void PosTerminalView_Loaded(object sender, System.Windows.RoutedEventArgs e)
@@ -16,6 +18,27 @@ namespace Axon.UI.Views
             if (DataContext is PosTerminalViewModel vm)
             {
                 await vm.LoadDataAsync();
+            }
+
+            // Automatically set focus on Barcode Scanner input box
+            TxtBarcodeInput?.Focus();
+        }
+
+        private void PosTerminalView_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // If user or scanner sends 'Enter' while not directly in a multiline box, trigger barcode processing
+            if (e.Key == Key.Enter || e.Key == Key.Return)
+            {
+                if (DataContext is PosTerminalViewModel vm)
+                {
+                    if (vm.ProcessBarcodeInputCommand.CanExecute(null))
+                    {
+                        vm.ProcessBarcodeInputCommand.Execute(null);
+                        TxtBarcodeInput?.Focus();
+                        TxtBarcodeInput?.SelectAll();
+                        e.Handled = true;
+                    }
+                }
             }
         }
     }
